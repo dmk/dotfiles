9f6ad16cfd6123752a1b3fc87ec5faa4581bc36e
Dimitry Koval <dkoval@heliostech.fr>
2016-10-12 18:35:10 +0300

    Add lockscreen

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -295,12 +295,13 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
+    awful.key({ modkey,           }, "l",     function () os.execute("dm-tool switch-to-greeter")    end),
+    awful.key({ modkey,           }, "j",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
     awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
+    awful.key({ modkey, "Shift"   }, "j",     function () awful.tag.incnmaster(-1)      end),
     awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
+    awful.key({ modkey, "Control" }, "j",     function () awful.tag.incncol(-1)         end),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
